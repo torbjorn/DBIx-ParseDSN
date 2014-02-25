efac5fd1a11bd8b20c5280bd8ab0f79c44e4d000
Torbjørn Lindahl <torbjorn.lindahl@gmail.com>
2014-02-25 02:02:10 +0100

    manifest

--- lib/DBIx/ParseDSN/Default.pm
+++ lib/DBIx/ParseDSN/Default.pm
@@ -10,7 +10,7 @@ use Carp qw< carp croak confess cluck >;
 use DBI; # will use parse_dsn from here
 use URI;
 
-use version; our $VERSION = qv('0.9.1');
+use version; our $VERSION = qv('0.9.2');
 
 use Moose;
 use MooseX::Aliases;
@@ -250,7 +250,7 @@ L<DBIx::ParseDSN/parse_dsn> which is the intended way to achieve this.
 
 =head1 VERSION
 
-This document describes DBIx::ParseDSN::Default version 0.9.1
+This document describes DBIx::ParseDSN::Default version 0.9.2
 
 =head1 SYNOPSIS
 
